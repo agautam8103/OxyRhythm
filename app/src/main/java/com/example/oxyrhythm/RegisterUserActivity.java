7e81458b61b6971914f1dec19d8f2a7d451ea919
Amritpal Parihar <amritspar@gmail.com>
2023-10-20 13:10:44 -0400

    Register User Page Complete

--- app/src/main/java/com/example/oxyrhythm/RegisterUserActivity.java
+++ app/src/main/java/com/example/oxyrhythm/RegisterUserActivity.java
@@ -1,14 +1,33 @@
 package com.example.oxyrhythm;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class RegisterUserActivity extends AppCompatActivity {
+public class RegisterUserActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        Spinner sex_choice = findViewById(R.id.sex_input);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Sex_Options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sex_choice.setAdapter(adapter);
+        sex_choice.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
