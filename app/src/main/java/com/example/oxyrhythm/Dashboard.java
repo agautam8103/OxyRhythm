290a6940af780ef7def3708af8e50e737b79bba8
agautam8103 <agautam8103@gmail.com>
2023-10-28 21:05:35 -0400

    Changes for get started page

---
+++ app/src/main/java/com/example/oxyrhythm/Dashboard.java
@@ -0,0 +1,17 @@
+package com.example.oxyrhythm;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Dashboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+    }
+}
