d5dc8d1519d3c8b70c55441dcb10d96a7c6d0010
agautam8103 <agautam8103@gmail.com>
2023-10-28 21:08:22 -0400

    UI changes for Register page

--- app/src/main/java/com/example/oxyrhythm/RegisterUserActivity.java
+++ app/src/main/java/com/example/oxyrhythm/RegisterUserActivity.java
@@ -1,12 +1,15 @@
 package com.example.oxyrhythm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class RegisterUserActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -15,7 +18,13 @@ public class RegisterUserActivity extends AppCompatActivity implements AdapterVi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
-
+        Button next = findViewById(R.id.save_usr_data_btn);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            dashboard();
+            }
+        });
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,11 +33,19 @@ public class RegisterUserActivity extends AppCompatActivity implements AdapterVi
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sex_choice.setAdapter(adapter);
         sex_choice.setOnItemSelectedListener(this);
+
+
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+    }
+
+    public void dashboard(){
+        Intent i = new Intent(this, Dashboard.class );
+        startActivity(i);
     }
 
     @Override
