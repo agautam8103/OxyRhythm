803c2c1d90635808dbbff5fdd6e17eb9ce1c037c
agautam8103 <agautam8103@gmail.com>
2023-10-29 23:39:02 -0400

    Adding Dashboard page and Heart Monitoring page

---
+++ app/src/main/java/com/example/oxyrhythm/HeartRate.java
@@ -0,0 +1,15 @@
+package com.example.oxyrhythm;
+
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HeartRate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.heartmonitor);
+    }
+}
