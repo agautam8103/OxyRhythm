65454d113d42aff46da1cb63a10ebdb1c997caf0
agautam8103 <agautam8103@gmail.com>
2023-11-12 01:34:37 -0500

    Dashboard page UI changes and new Health Monitor page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
 }
 
+
+
 android {
     namespace = "com.example.oxyrhythm"
     compileSdk = 34
@@ -31,6 +33,8 @@ android {
     }
 }
 
+
+
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -39,4 +43,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.robinhood.spark:spark:1.2.0")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 }
