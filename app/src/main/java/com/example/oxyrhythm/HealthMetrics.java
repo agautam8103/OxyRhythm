a42428a03634c41e5e0482cbb3d30b524233b003
Amritpal Parihar <amritspar@gmail.com>
2023-11-12 22:43:06 -0500

    Init of Health Metrics Page

--- app/src/main/java/com/example/oxyrhythm/HealthMetrics.java
+++ app/src/main/java/com/example/oxyrhythm/HealthMetrics.java
@@ -1,15 +1,34 @@
 package com.example.oxyrhythm;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HealthMetrics extends AppCompatActivity {
 
+    private OxyUser oxy_user;
+    private DataBase oxy_user_saved_data;
+    TextView height_val, weight_val;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.healthmetrics);
+        setContentView(R.layout.healthmetrics);
+
+        oxy_user_saved_data = new DataBase(HealthMetrics.this);
+        height_val = findViewById(R.id.Height_val);
+        weight_val = findViewById(R.id.Weight_val);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        oxy_user = oxy_user_saved_data.getSavedOxyUser();
+
+        height_val.setText("Height: " + oxy_user.getHeight());
+        weight_val.setText("Weight: " + oxy_user.getWeight());
     }
 }
