8d9d7489c71440405e3b2f02e124df5305c4ecbb
Amritpal Parihar <amritspar@gmail.com>
2023-11-16 21:20:45 -0500

    Made Visibility Control on Dashboard Page's Components Based on if User Registered

--- app/src/main/java/com/example/oxyrhythm/Dashboard.java
+++ app/src/main/java/com/example/oxyrhythm/Dashboard.java
@@ -16,6 +16,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,7 +37,10 @@ public class Dashboard extends AppCompatActivity {
 
     private OxyUser oxy_user;
     private DataBase oxy_user_saved_data;
-    TextView greeting_oxy_user, dash_mesg;
+    TextView greeting_oxy_user, dash_mesg, heart_rate_label, blood_oxygen_level_label, body_temperature_label,
+            Click_to_see_more_label1, Click_to_see_more_label2, Click_to_see_more_label3;
+    Button heart_rate_BTN, blood_oxy_BTN, body_temp_BTN, health_metics_BTN, help_BTN;
+    ImageView heart_pic, temp_pic;
 //    private BluetoothGatt bluetoothGatt;
 //    private BluetoothAdapter bluetoothAdapter;
 //    private boolean bluetoothConnected = false;
@@ -243,27 +247,27 @@ public class Dashboard extends AppCompatActivity {
 //        setLineChartData(lineChart, dataforheartrate, "#FF0000");
 //        setLineChartData(lineChart2, dataforbloodoxygen, "#0000FF");
 
-        Button heart_rate_BTN = findViewById(R.id.heart_rate_btn);
+        heart_rate_BTN = findViewById(R.id.heart_rate_btn);
         heart_rate_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {heartRate();}});
         
-        Button blood_oxy_BTN = findViewById(R.id.blood_oxygen_btn);
+        blood_oxy_BTN = findViewById(R.id.blood_oxygen_btn);
         blood_oxy_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {bloodOxygenLevel();}});
         
-        Button body_temp_BTN = findViewById(R.id.body_temperature_btn);
+        body_temp_BTN = findViewById(R.id.body_temperature_btn);
         body_temp_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {temperature();}});
         
-        Button health_metics_BTN = findViewById(R.id.health_metric_btn);
+        health_metics_BTN = findViewById(R.id.health_metric_btn);
         health_metics_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {healthMetrics();}});
         
-        Button help_BTN = findViewById(R.id.help_btn);
+        help_BTN = findViewById(R.id.help_btn);
         help_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {help();}});
@@ -271,6 +275,15 @@ public class Dashboard extends AppCompatActivity {
         oxy_user_saved_data = new DataBase(Dashboard.this);
         greeting_oxy_user = findViewById(R.id.hello_OxyUser_label);
         dash_mesg = findViewById(R.id.dash_mesg);
+        heart_rate_label = findViewById(R.id.heart_rate_label);
+        blood_oxygen_level_label = findViewById(R.id.blood_oxygen_level_label);
+        body_temperature_label = findViewById(R.id.body_temperature_label);
+        Click_to_see_more_label1 = findViewById(R.id.Click_to_see_more_label1);
+        Click_to_see_more_label2 = findViewById(R.id.Click_to_see_more_label2);
+        Click_to_see_more_label3 = findViewById(R.id.Click_to_see_more_label3);
+
+        heart_pic = findViewById(R.id.Heart_Pic);
+        temp_pic = findViewById(R.id.Temp_pic);
     }
 
 //    private void setLineChartData(LineChart chart, float[] data, String lineColor) {
@@ -306,35 +319,59 @@ public class Dashboard extends AppCompatActivity {
         oxy_user = oxy_user_saved_data.getSavedOxyUser();
 
         if (oxy_user.OxyUserIsEmpty()) {
+            heart_rate_BTN.setVisibility(View.INVISIBLE);
+            blood_oxy_BTN.setVisibility(View.INVISIBLE);
+            body_temp_BTN.setVisibility(View.INVISIBLE);
+            health_metics_BTN.setVisibility(View.INVISIBLE);
+            help_BTN.setVisibility(View.INVISIBLE);
+
+            heart_pic.setVisibility(View.INVISIBLE);
+            temp_pic.setVisibility(View.INVISIBLE);
+
             Intent i = new Intent(this, GetStartedActivity.class);
             startActivity(i);
         } else {
             greeting_oxy_user.setText("Hello, " + oxy_user.getFirstName() + "!");
             dash_mesg.setText("Time to check up on your health routine");
+            heart_rate_label.setText("Heart Rate");
+            blood_oxygen_level_label.setText("Blood Oxygen Level");
+            body_temperature_label.setText("Body Temperature");
+            Click_to_see_more_label1.setText("Click for more info.");
+            Click_to_see_more_label2.setText("Click for more info.");
+            Click_to_see_more_label3.setText("Click for more info.");
+
+            heart_rate_BTN.setVisibility(View.VISIBLE);
+            blood_oxy_BTN.setVisibility(View.VISIBLE);
+            body_temp_BTN.setVisibility(View.VISIBLE);
+            health_metics_BTN.setVisibility(View.VISIBLE);
+            help_BTN.setVisibility(View.VISIBLE);
+
+            heart_pic.setVisibility(View.VISIBLE);
+            temp_pic.setVisibility(View.VISIBLE);
         }
     }
 
-    public void heartRate() {
+    private void heartRate() {
         Intent i = new Intent(this, HeartRate.class);
         startActivity(i);
     }
 
-    public void bloodOxygenLevel() {
+    private void bloodOxygenLevel() {
         Intent i = new Intent(this, BloodOxygenLevel.class);
         startActivity(i);
     }
 
-    public void temperature() {
+    private void temperature() {
         Intent i = new Intent(this, Temperature.class);
         startActivity(i);
     }
 
-    public void healthMetrics() {
+    private void healthMetrics() {
         Intent i = new Intent(this, HealthMetrics.class);
         startActivity(i);
     }
 
-    public void help() {
+    private void help() {
         Intent i = new Intent(this, Help.class);
         startActivity(i);
     }
