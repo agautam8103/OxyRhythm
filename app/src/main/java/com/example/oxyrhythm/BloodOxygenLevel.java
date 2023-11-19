9995d5ec65bbe2f376a90c0e3ac4f2a7738ced62
SahanaCoding <dsahana10@hotmail.com>
2023-11-19 00:57:42 -0500

    Fixed U-i PAGES added help page

--- app/src/main/java/com/example/oxyrhythm/BloodOxygenLevel.java
+++ app/src/main/java/com/example/oxyrhythm/BloodOxygenLevel.java
@@ -1,8 +1,18 @@
 package com.example.oxyrhythm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class BloodOxygenLevel extends Dashboard {
 
@@ -13,6 +23,61 @@ public class BloodOxygenLevel extends Dashboard {
 
         setSupportActionBar(findViewById(R.id.toolbar2));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Get the LineChart reference from your layout
+        LineChart lineChart = findViewById(R.id.temperaturechart);
+
+        // Create a method to set up your LineChart and populate it with data
+        setupLineChart(lineChart);
+    }
+
+    private void setupLineChart(LineChart lineChart) {
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 96));
+        entries.add(new Entry(2, 97));
+        entries.add(new Entry(3, 97));
+        entries.add(new Entry(4, 96));
+        entries.add(new Entry(5, 100));
+        entries.add(new Entry(6, 97));
+
+
+        LineDataSet dataSet = new LineDataSet(entries, "SpO2 Levels");
+
+
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setCircleColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setDrawValues(true);
+
+
+        LineData data = new LineData(dataSet);
+
+
+        lineChart.setData(data);
+        lineChart.getDescription().setText("Blood Oxygen Monitoring");  // Set description label
+        lineChart.animateY(2000);  // Add animation
+
+        // Customize the X and Y axes
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]{"Time 1", "Time 2", "Time"}));
+        xAxis.setDrawGridLines(false); // Disable vertical grid lines
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setLabelCount(6, true); // Set the number of labels on the Y-axis
+        leftAxis.setDrawGridLines(false); // Disable horizontal grid lines
+
+        YAxis rightAxis = lineChart.getAxisRight();
+        rightAxis.setEnabled(false);  // Disable the right Y axis
+
+        // Customize the legend
+        Legend legend = lineChart.getLegend();
+        legend.setForm(Legend.LegendForm.LINE);
+
+        // Refresh the chart to display the changes
+        lineChart.invalidate();
+
+
+
     }
 
     @Override
--- app/src/main/java/com/example/oxyrhythm/Temperature.java
+++ app/src/main/java/com/example/oxyrhythm/Temperature.java
@@ -4,6 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class Temperature extends Dashboard {
 
     @Override
@@ -13,6 +25,62 @@ public class Temperature extends Dashboard {
 
         setSupportActionBar(findViewById(R.id.toolbar2));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get the LineChart reference from your layout
+        LineChart lineChart = findViewById(R.id.temperaturechart);
+
+        // Create a method to set up your LineChart and populate it with data
+        setupLineChart(lineChart);
+    }
+
+    private void setupLineChart(LineChart lineChart) {
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 34));
+        entries.add(new Entry(2, 35));
+        entries.add(new Entry(3, 27));
+        entries.add(new Entry(4, 36));
+        entries.add(new Entry(5, 30));
+        entries.add(new Entry(6, 28));
+
+
+        LineDataSet dataSet = new LineDataSet(entries, "Body Temperature");
+
+
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setCircleColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setDrawValues(true);
+
+
+        LineData data = new LineData(dataSet);
+
+
+        lineChart.setData(data);
+        lineChart.getDescription().setText("Body Temperature Monitoring");  // Set description label
+        lineChart.animateY(2000);  // Add animation
+
+        // Customize the X and Y axes
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]{"Time 1", "Time 2", "Time"}));
+        xAxis.setDrawGridLines(false); // Disable vertical grid lines
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setLabelCount(6, true); // Set the number of labels on the Y-axis
+        leftAxis.setDrawGridLines(false); // Disable horizontal grid lines
+
+        YAxis rightAxis = lineChart.getAxisRight();
+        rightAxis.setEnabled(false);  // Disable the right Y axis
+
+        // Customize the legend
+        Legend legend = lineChart.getLegend();
+        legend.setForm(Legend.LegendForm.LINE);
+
+        // Refresh the chart to display the changes
+        lineChart.invalidate();
+
+
+
     }
 
     @Override
